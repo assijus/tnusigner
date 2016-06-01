64c19ebeabea3ac33b27f818ee4b849c924ff5e1
Renato Crivano <renato.crivano@gmail.com>
2016-06-01 12:38:30 -0300

    Forçando o CPF e ignorando os já assinados.

--- src/main/resources/br/jus/trf2/tnu/signer/list.sql
+++ src/main/resources/br/jus/trf2/tnu/signer/list.sql
@@ -2,4 +2,4 @@ select	pd.documentoId, pd.processoId,
 concat(SUBSTRING(pro.numeroProcesso, 1, 7), '-', SUBSTRING(pro.numeroProcesso, 8, 2), '.',SUBSTRING(pro.numeroProcesso, 10, 4),'.',SUBSTRING(pro.numeroProcesso, 14, 1), '.',SUBSTRING(pro.numeroProcesso, 15, 2), '.',SUBSTRING(pro.numeroProcesso, 17, 4)) as numeroProcesso, 
 pd.descricao, pd.tipoDocumentoDescricao
 from ProcessoDocumento pd left outer join Processo pro on pd.processoid = pro.id
-where	pd.cpf = ? or pd.cpf is null
+where pd.cpf = ? and pd.attached is not null
