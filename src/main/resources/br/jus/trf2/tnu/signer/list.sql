5a8bc89c2922b1f406662a1d7a110c65ad8590a5
André Luís <andre.silva@jfrj.jus.br>
2016-06-08 17:54:34 -0300

    Converte a query que lista os documentos pendentes de assinatura do sqlServer para o Oracle

--- src/main/resources/br/jus/trf2/tnu/signer/list.sql
+++ src/main/resources/br/jus/trf2/tnu/signer/list.sql
@@ -1,5 +1,5 @@
 select	pd.documentoId, pd.processoId,
-concat(SUBSTRING(pro.numeroProcesso, 1, 7), '-', SUBSTRING(pro.numeroProcesso, 8, 2), '.',SUBSTRING(pro.numeroProcesso, 10, 4),'.',SUBSTRING(pro.numeroProcesso, 14, 1), '.',SUBSTRING(pro.numeroProcesso, 15, 2), '.',SUBSTRING(pro.numeroProcesso, 17, 4)) as numeroProcesso, 
+SUBSTR(pro.numeroProcesso, 1, 7) || '-' || SUBSTR(pro.numeroProcesso, 8, 2) || '.' || SUBSTR(pro.numeroProcesso, 10, 4) || '.' || SUBSTR(pro.numeroProcesso, 14, 1) || '.' || SUBSTR(pro.numeroProcesso, 15, 2) || '.' || SUBSTR(pro.numeroProcesso, 17, 4) as numeroProcesso,
 pd.descricao, pd.tipoDocumentoDescricao
 from ProcessoDocumento pd left outer join Processo pro on pd.processoid = pro.id
-where pd.cpf = ? and pd.attached is not null
+where pd.cpf = ? and pd.attached is null
