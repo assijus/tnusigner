0c53631ab7fede2e2cb823893bd17c493d998c44
André Luís <andre.silva@jfrj.jus.br>
2016-06-09 18:23:15 -0300

    Lista somente processos que estão na situação 2 (Liberado para assinar)

--- src/main/resources/br/jus/trf2/tnu/signer/list.sql
+++ src/main/resources/br/jus/trf2/tnu/signer/list.sql
@@ -2,4 +2,4 @@ select	pd.documentoId, pd.processoId,
 SUBSTR(pro.numeroProcesso, 1, 7) || '-' || SUBSTR(pro.numeroProcesso, 8, 2) || '.' || SUBSTR(pro.numeroProcesso, 10, 4) || '.' || SUBSTR(pro.numeroProcesso, 14, 1) || '.' || SUBSTR(pro.numeroProcesso, 15, 2) || '.' || SUBSTR(pro.numeroProcesso, 17, 4) as numeroProcesso,
 pd.descricao, pd.tipoDocumentoDescricao
 from ProcessoDocumento pd left outer join Processo pro on pd.processoid = pro.id
-where pd.cpf = ? and pd.attached is null
+where pd.cpf = ? and pd.attached is null and pro.situacao = 2
