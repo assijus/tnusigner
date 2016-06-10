5e67d787a448ee6fa6a6f0482c8bf83f335bcc94
André Luís <andre.silva@jfrj.jus.br>
2016-06-10 17:21:06 -0300

    Busca descrição na tabela de tipos de documento

--- src/main/resources/br/jus/trf2/tnu/signer/list.sql
+++ src/main/resources/br/jus/trf2/tnu/signer/list.sql
@@ -1,5 +1,6 @@
 select	pd.documentoId, pd.processoId,
 SUBSTR(pro.numeroProcesso, 1, 7) || '-' || SUBSTR(pro.numeroProcesso, 8, 2) || '.' || SUBSTR(pro.numeroProcesso, 10, 4) || '.' || SUBSTR(pro.numeroProcesso, 14, 1) || '.' || SUBSTR(pro.numeroProcesso, 15, 2) || '.' || SUBSTR(pro.numeroProcesso, 17, 4) as numeroProcesso,
-pd.descricao, pd.tipoDocumentoDescricao
-from ProcessoDocumento pd left outer join Processo pro on pd.processoid = pro.id
+pd.descricao, tp.descricao as tipoDocumentoDescricao
+from ProcessoDocumento pd left outer join Processo pro on pd.processoid = pro.id 
+left outer join tiposdocumentoprocessual tp on pd.tipodocumentoid = tp.codigo
 where pd.cpf = ? and pd.attached is null and pro.situacao = 2
