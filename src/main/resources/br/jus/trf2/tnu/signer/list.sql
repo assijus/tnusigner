ce2d21d6b60ab93728b33cf270926155d7eb2269
André Luís <andre.silva@jfrj.jus.br>
2016-06-23 17:03:18 -0300

    Altera query que lista os documentos

--- src/main/resources/br/jus/trf2/tnu/signer/list.sql
+++ src/main/resources/br/jus/trf2/tnu/signer/list.sql
@@ -3,4 +3,5 @@ SUBSTR(pro.numeroProcesso, 1, 7) || '-' || SUBSTR(pro.numeroProcesso, 8, 2) || '
 pd.descricao, tp.descricao as tipoDocumentoDescricao
 from ProcessoDocumento pd left outer join Processo pro on pd.processoid = pro.id 
 left outer join tiposdocumentoprocessual tp on pd.tipodocumentoid = tp.codigo
-where pd.cpf = ? and pd.attached is null and pro.situacao = 2
+left outer join usuario usu on usu.idorgao = pro.idorgao
+where usu.cpf = ? and usu.assinadoc = 'S' and pd.attached is null and pro.situacao = 2
