f6abd70bfa87324a5b89e3057be0e84da296c1a4
André Luís <andre.silva@jfrj.jus.br>
2016-06-23 17:30:44 -0300

    Corrige Formatação do número do processo

--- src/main/resources/br/jus/trf2/tnu/signer/list.sql
+++ src/main/resources/br/jus/trf2/tnu/signer/list.sql
@@ -1,5 +1,5 @@
 select	pd.documentoId, pd.processoId,
-SUBSTR(pro.numeroProcesso, 1, 7) || '-' || SUBSTR(pro.numeroProcesso, 8, 2) || '.' || SUBSTR(pro.numeroProcesso, 10, 4) || '.' || SUBSTR(pro.numeroProcesso, 14, 1) || '.' || SUBSTR(pro.numeroProcesso, 15, 2) || '.' || SUBSTR(pro.numeroProcesso, 17, 4) as numeroProcesso,
+pro.numerocommascara as numeroProcesso,
 pd.descricao, tp.descricao as tipoDocumentoDescricao
 from ProcessoDocumento pd left outer join Processo pro on pd.processoid = pro.id 
 left outer join tiposdocumentoprocessual tp on pd.tipodocumentoid = tp.codigo
